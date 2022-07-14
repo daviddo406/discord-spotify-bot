6a615a4a6b9584b41a74859b51cd123d4580e3a0
daviddo406 <phamdavid102@gmail.com>
2022-07-14 18:39:02 -0400

    Initial commit

---
+++ discord-spotify-bot/Modules/ReplyModule.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_Spotify_Bot.Modules
+{
+    public class ReplyModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("say")]
+        [Summary("Echos a message")]
+        public Task SayAsync([Remainder][Summary("The text to echo")] string echo) 
+            => ReplyAsync(echo);
+    }
+}
---
+++ discord-spotify-bot/Program.cs
@@ -0,0 +1,75 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_Spotify_Bot
+{
+    public class Program
+    {
+        // Program entry point
+        static Task Main(string[] args)
+        {
+            return new Program().MainAsync();
+        }
+
+        private DiscordSocketClient _client;
+
+        private readonly CommandHandler _commandHandler;
+        private readonly CommandService _commands;
+        //todo: research
+        //private readonly IServiceProvider _services;
+
+        private readonly LoggingService _loggingService;
+        private Program()
+        {
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Info
+            });
+
+            _commands = new CommandService(new CommandServiceConfig
+            {
+                CaseSensitiveCommands = false
+            });
+
+            _loggingService = new LoggingService(_client, _commands);
+
+            _commandHandler = new CommandHandler(_client, _commands);
+            // Setup your DI container.
+            //_services = ConfigureServices();
+        }
+
+        private async Task MainAsync()
+        {
+            await _commandHandler.InstallCommandsAsync();
+
+            var token = "OTk3MjY2ODg3OTk2MDg4NDQx.GcsKsy.32lMsXoF1sVVtj17RFunTiSB1S9yLlNDh2kJz4";
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Run(EndMain);
+        }
+
+        private Task EndMain()
+        {
+            string hostInput;
+            do
+            {
+                hostInput = Console.ReadLine().ToLower();
+            } 
+            while ((String.Equals(hostInput, "stop") == false));
+
+            Console.WriteLine("Ending Main...");
+            _client.StopAsync();
+            return Task.CompletedTask;
+        }
+    }
+}
