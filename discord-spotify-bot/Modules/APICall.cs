6d261de0f7edad55e0998997069a02fcd4bfb0ba
ssc84 <ssc84@drexel.edu>
2022-08-10 18:38:19 -0400

    [ DB-24 ] Make token dynamic (OAuth)
    Fixed no spotify token error

--- discord-spotify-bot/Modules/APICall.cs
+++ discord-spotify-bot/Modules/APICall.cs
@@ -8,26 +8,45 @@ using Newtonsoft.Json;
 using Discord_Spotify_Bot.Models;
 using System.IO;
 
-//Token Documentation https://johnnycrazy.github.io/SpotifyAPI-NET/docs/client_credentials
+
 namespace Discord_Spotify_Bot.Modules
 {
         public class APICall : ModuleBase<SocketCommandContext>
-    {
+        {
+        int errorCounter = 0;
 
         [Command("tracks")]
         [Summary("get tracks, this is for api testing")]
         public async Task GetTracksAsync(){
             using (var client = new HttpClient()){ // using keyword is used for closing the httpClient connection when no longer in use https://www.automationmission.com/2020/02/15/clean-up-your-net-with-the-dispose-pattern/
                 var id = "12uzJ2TWi4IoxssgaNY8MO";
-                var token = File.ReadAllText(@".\spotifyToken.txt");
+                string token = "";
+                try
+                {
+                    token = File.ReadAllText(@".\spotifyToken.txt");
+                }
+                catch (FileNotFoundException)
+                {
+                    await RefreshSpotifyToken.GetTokenAsync();
+                }
                 var endpoint = client.BaseAddress = new Uri($"https://api.spotify.com/v1/tracks/{id}");
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
                 var result = client.GetAsync(endpoint).Result;
                 if (result.IsSuccessStatusCode) {
                 SpotifySongDetail json = result.Content.ReadAsAsync<SpotifySongDetail>().Result;
+                await ReplyAsync("https://open.spotify.com/track/7a86XRg84qjasly9f6bPSD?si=cd0cdb460741405e");
                 }
                 else {
-                    await RefreshSpotifyToken.GetTokenAsync();
+                    if (errorCounter == 0)
+                    {
+                        errorCounter++;
+                        await RefreshSpotifyToken.GetTokenAsync();
+                        await GetTracksAsync();
+                    }
+                    else
+                    {
+                        await ReplyAsync("We are sorry. Looks like something went wrong with our server, our Devs are on the case!");
+                    }
                 }
         }
         }
